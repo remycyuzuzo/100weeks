337cf7ca32b350140fcbd25428c837fb95fc41fa
CYUZUZO J. Remy <remycyuzuzo@gmail.com>
2021-11-07 22:59:55 +0200

    added better styles to original files, and created some others

---
+++ admin/dependencies.php
@@ -0,0 +1,9 @@
+<?php
+/** project url */
+define('URL', 'http://localhost/100weeks', true);
+
+/** path of bootstrap.min.css file, replace this during production with remote package */
+define("BOOTSTRAP_CSS", URL."/node_modules/bootstrap/dist/css/bootstrap.min.css", true);
+
+/** path of bootstrap.min.js file, replace this during production with remote package */
+define("BOOTSTRAP_JS", URL."/node_modules/bootstrap/dist/js/bootstrap.min.js", true);
--- admin/manage-sessions.php
+++ admin/manage-sessions.php
@@ -1,9 +1,8 @@
 <?php
-include "./admin/dependencies.php";
 
 # check whether the session/cookie is set, if not, redirect the page to the login page
 if (isset($_SESSION['logged_in'])) {
-    echo "<script>window.location='".URL."'/admin/dashboard.php</script>";
+    
 } else {
     echo "<script>window.location='".URL."'/admin/login.php</script>";
 }
