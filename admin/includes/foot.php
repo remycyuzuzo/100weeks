be3a9c4d4efe8ffaff1e1b6c6ac7674147a82e20
CYUZUZO J. Remy <remycyuzuzo@gmail.com>
2022-04-20 15:36:52 +0200

    Added the foot file

---
+++ admin/includes/foot.php
@@ -0,0 +1,11 @@
+<!-- footer -->
+<?php include __DIR__ . "/footer.php" ?>
+<!-- ./end of footer -->
+</div>
+
+<!-- JAVASCRIPT FILES -->
+<script src="<?= URL ?>/res/js/admin-sidebar.js"></script>
+<script src="<?= BOOTSTRAP_JS ?>"></script>
+</body>
+
+</html>
